ed27ad11cc2317e29041ada9eefbebe6711970b7
Xiahui Zhang <xhz0729@gmail.com>
2024-07-05 00:23:58 -0500

    Added solution to high-score-board

--- high-score-board.js
+++ high-score-board.js
@@ -1,66 +1,97 @@
-/// <reference path="./global.d.ts" />
-// @ts-check
+// // <reference path="./global.d.ts" />
+// // @ts-check
 
-/**
- * Creates a new score board with an initial entry.
- *
- * @returns {Record<string, number>} new score board
- */
-export function createScoreBoard() {
-  throw new Error('Please implement the createScoreBoard function');
+// /**
+//  * Creates a new score board with an initial entry.
+//  *
+//  * @returns {Record<string, number>} new score board
+//  */
+function createScoreBoard() {
+  const scoreBoard = {
+    'The-Best-Ever': 1000000
+  }
+  return scoreBoard;
 }
 
-/**
- * Adds a player to a score board.
- *
- * @param {Record<string, number>} scoreBoard
- * @param {string} player
- * @param {number} score
- * @returns {Record<string, number>} updated score board
- */
-export function addPlayer(scoreBoard, player, score) {
-  throw new Error('Please implement the addPlayer function');
+console.log(createScoreBoard()); // returns an object with one initial entry
+// /**
+//  * Adds a player to a score board.
+//  *
+//  * @param {Record<string, number>} scoreBoard
+//  * @param {string} player
+//  * @param {number} score
+//  * @returns {Record<string, number>} updated score board
+//  */
+function addPlayer(scoreBoard, player, score) {
+  scoreBoard[player] = score;
+  return scoreBoard;
+}
+console.log(addPlayer({ 'Dave Thomas': 0 }, 'José Valim', 486373)); // => {'Dave Thomas': 0, 'José Valim': 486373}
+
+// /**
+//  * Removes a player from a score board.
+//  *
+//  * @param {Record<string, number>} scoreBoard
+//  * @param {string} player
+//  * @returns {Record<string, number>} updated score board
+//  */
+function removePlayer(scoreBoard, player) {
+  delete scoreBoard[player];
+  return scoreBoard;
+}
+console.log(removePlayer({ 'Dave Thomas': 0 }, 'Dave Thomas')); // => {}
+
+console.log(removePlayer({ 'Dave Thomas': 0 }, 'Rose Fanaras')); // => { 'Dave Thomas': 0 })
+
+// /**
+//  * Increases a player's score by the given amount.
+//  *
+//  * @param {Record<string, number>} scoreBoard
+//  * @param {string} player
+//  * @param {number} points
+//  * @returns {Record<string, number>} updated score board
+//  */
+function updateScore(scoreBoard, player, points) {
+  scoreBoard[player] = scoreBoard[player] + points;
+  return scoreBoard;
+}
+console.log(updateScore({ 'Freyja Ćirić': 12771008 }, 'Freyja Ćirić', 73)); // => {"Freyja Ćirić", 12771081}
+
+// /**
+//  * Applies 100 bonus points to all players on the board.
+//  *
+//  * @param {Record<string, number>} scoreBoard
+//  * @returns {Record<string, number>} updated score board
+//  */
+function applyMondayBonus(scoreBoard) {
+  for (let key in scoreBoard) {
+    scoreBoard[key] = scoreBoard[key] + 100;
+  }
+  return scoreBoard;
+}
+const scoreBoard = {
+  'Dave Thomas': 44,
+  'Freyja Ćirić': 539,
+  'José Valim': 265,
+};
+
+console.log(applyMondayBonus(scoreBoard));// => { 'Dave Thomas': 144, 'Freyja Ćirić': 639, 'José Valim': 365 }
+
+// /**
+//  * Normalizes a score with the provided normalization function.
+//  *
+//  * @param {Params} params the parameters for performing the normalization
+//  * @returns {number} normalized score
+//  */
+function normalize(score) {
+  return 2 * score + 10;
 }
 
-/**
- * Removes a player from a score board.
- *
- * @param {Record<string, number>} scoreBoard
- * @param {string} player
- * @returns {Record<string, number>} updated score board
- */
-export function removePlayer(scoreBoard, player) {
-  throw new Error('Please implement the removePlayer function');
-}
+function normalizeScore(params) {
+   let val = params['score']
+   return normalize(val);
 
-/**
- * Increases a player's score by the given amount.
- *
- * @param {Record<string, number>} scoreBoard
- * @param {string} player
- * @param {number} points
- * @returns {Record<string, number>} updated score board
- */
-export function updateScore(scoreBoard, player, points) {
-  throw new Error('Please implement the updateScore function');
 }
+const params = { score: 400, normalizeFunction: normalize };
+console.log(normalizeScore(params)); // => 810
 
-/**
- * Applies 100 bonus points to all players on the board.
- *
- * @param {Record<string, number>} scoreBoard
- * @returns {Record<string, number>} updated score board
- */
-export function applyMondayBonus(scoreBoard) {
-  throw new Error('Please implement the applyMondayBonus function');
-}
-
-/**
- * Normalizes a score with the provided normalization function.
- *
- * @param {Params} params the parameters for performing the normalization
- * @returns {number} normalized score
- */
-export function normalizeScore(params) {
-  throw new Error('Please implement the normalizeScore function');
-}
